fb597b6c29ad0ab85ec52e9891a4b4efcd738576
shaque525 <40909491+shaque525@users.noreply.github.com>
2022-08-31 23:39:15 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import sampleData from './form.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proj-formIO';
+  submittedData: any;
+
+  customForm = sampleData;
+
+  onSubmit(event: any) {
+    this.submittedData = event;
+  }
 }
